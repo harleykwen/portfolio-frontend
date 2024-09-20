126a71c4773d0b80b5d5fb0f4aa0b3dcb1f812e6
harleykwen <harleykwen.work@gmail.com>
2024-09-20 13:26:52 +0700

    feat: login

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import theme from '@theme/theme.ts'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ChakraProvider theme={theme}>
             <RouteProvider>
-                <App />
+                <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_KEY??''}>
+                    <App />
+                </GoogleOAuthProvider>
             </RouteProvider>
         </ChakraProvider>
     </StrictMode>,
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import loginBg from '@assets/backgrounds/login_bg.svg'
+import { Flex } from '@chakra-ui/react'
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google'
 
 const Login: React.FC = () => {
     return (
-        <div>Login</div>
+        <Flex 
+            height='100vh'
+            backgroundImage={`url(${loginBg})`}
+            backgroundSize='cover'
+            backgroundPosition='center'
+            backgroundColor='#2148c0'
+            justifyContent='center'
+            alignItems='center'
+        >
+            <GoogleLogin
+                onSuccess={(credentialResponse: CredentialResponse) => {
+                    console.log(credentialResponse)
+                }}
+                onError={() => {
+                    console.log('error')
+                }}
+            />
+        </Flex>
     )
 }
 
